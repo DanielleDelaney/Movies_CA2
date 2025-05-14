7cc5ed908c369312a67cdad9b80c001c8cee5134
DanielleDelaney <sba24413@student.cct.ie>
2025-05-14 17:41:04 +0100

    Update Movies_DV_1.py

--- Movies_DV_1.py
+++ Movies_DV_1.py
@@ -1,20 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#import plotly.express as px
-#import warnings
-
-#from sklearn.metrics.pairwise import cosine_similarity
-#from sklearn.feature_extraction.text import TfidfVectorizer
-#from sklearn.decomposition import PCA
 
 st.set_page_config(page_title="Streamlit Data App", layout="wide")
-
-warnings.filterwarnings("ignore")
-
-sns.set_context("talk")
 
 
 st.title("Movies Analysis")
