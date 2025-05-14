037212a7721909940c47259ece4062575e6afcf6
DanielleDelaney <sba24413@student.cct.ie>
2025-05-14 17:39:04 +0100

    Update Movies_DV_1.py

--- Movies_DV_1.py
+++ Movies_DV_1.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import warnings
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#import plotly.express as px
+#import warnings
 
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import PCA
+#from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.decomposition import PCA
 
 st.set_page_config(page_title="Streamlit Data App", layout="wide")
 
